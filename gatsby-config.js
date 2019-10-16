619264412635506cbac59d2e9944cbaec090ffa9
anielakm <a.katanamatlok@gmail.com>
2019-10-16 09:39:19 +0200

    added title and app icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Anielakm`,
+    description: `Aniela Katana Matłok | Junior Front-End Developer Portfolio`,
+    author: `@anielakm`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
